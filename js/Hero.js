a67b32934cb419148153d2605927d2010d4d3103
Haritakis Papadimitrakis <haritakis116@gmail.com>
2026-04-28 17:52:14 +0300

    Add hero object with initial properties

--- js/Hero.js
+++ js/Hero.js
@@ -1,1 +1,23 @@
+let hero = {
 
+  name: "Haritos",
+
+  level: 1,
+
+  xp: 0,
+
+  hp: 100,
+
+  maxHp: 100,
+
+  attack: 10,
+
+  defense: 5,
+
+  gold: 0,
+
+  inventory: [],
+
+  skills: []
+
+};
